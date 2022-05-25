2269f711c6e60cbe5a350b2d994a4c1c1ca41130
CheezeStick <104651918+CheezeStick@users.noreply.github.com>
2022-05-25 12:00:37 -0700

    no

--- AstroMonkey/Assets/Scripts/PlayerMove.cs
+++ AstroMonkey/Assets/Scripts/PlayerMove.cs
@@ -7,26 +7,17 @@ public class PlayerMove : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     public float speed = 5.0f;
-    public float upForce = 10f;
-
-    private Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>(); 
+    
     }
 
     // Update is called once per frame
     void Update()
     {
        horizontal = Input.GetAxis("Horizontal");
-
-       if (Input.GetButtonDown("Space"))
-        {
-            rb2d.velocity = Vector2.zero;
-            rb2d.AddForce(new Vector2(0, upForce));
-        }
 
     }
 
