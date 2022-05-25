cc15731d4df1c6ac80a67cd60debfcbff9009c94
CheezeStick <104651918+CheezeStick@users.noreply.github.com>
2022-05-25 16:40:36 -0700

    Revert "clean"
    This reverts commit 5fe95a38376c3570fb27ba10a5f9a43c1322f219.

--- AstroMonkey/Assets/Scripts/PlayerMove.cs
+++ AstroMonkey/Assets/Scripts/PlayerMove.cs
@@ -6,17 +6,25 @@ public class PlayerMove : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
     float horizontal;
-    public float speed = 5.0f; 
+    public float speed = 5.0f;
+    public float jumpForce = 20f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
+       horizontal = Input.GetAxis("Horizontal");
+
+       if (Input.GetButtonDown("Jump"))
+        {
+            Jump(); 
+        }
+
     }
 
     private void FixedUpdate()
@@ -25,5 +33,13 @@ public class PlayerMove : MonoBehaviour
         position.x = position.x + speed * horizontal * Time.deltaTime;
 
         rigidbody2d.MovePosition(position); 
+        
+    }
+
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rigidbody2d.velocity.x, jumpForce);
+
+        rigidbody2d.velocity = movement; 
     }
 }
